410d8cceb1b48797aba849c52c88a3e37136e78c
Caue Carvalho <cauefelipe1@hotmail.com>
2024-07-09 11:02:09 -0300

    simple component and demos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 //import logo from './logo.svg';
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
+  const showEmployees = true;
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <p> Hello </p>
-      </header>
+      {console.log("We are about to list employees.")}
+      {showEmployees ? 
+        <>
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+        </>
+        : 
+        <p>You cannot see the employess</p>}
+
     </div>
   );
 }
