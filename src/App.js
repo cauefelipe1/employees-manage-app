4c17fdbc3edaca0125d0cf37869988f419f06d63
Caue Carvalho <cauefelipe1@hotmail.com>
2024-07-09 14:42:49 -0300

    About to add tailwind

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 //import logo from './logo.svg';
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState("dev");
   const showEmployees = true;
   
   return (
     <div className="App">
-      {console.log("We are about to list employees.")}
       {showEmployees ? 
         <>
+          <input type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+          }}/>
+
           <Employee name="Steve" role="Intern"/>
-          <Employee name="Roger"/>
+          <Employee name="Roger" role={ role }/>
           <Employee name="Erin"/>
           <Employee name="Mary"/>
         </>
