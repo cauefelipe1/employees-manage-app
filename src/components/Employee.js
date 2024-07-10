62f73a6a868a7fedf44f7fa57ef73f65564875e4
Caue Carvalho <cauefelipe1@hotmail.com>
2024-07-10 11:55:38 -0300

    Adding min and max width for employee card

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -2,7 +2,7 @@ import EditEmployee from "./EditEmployee";
 
 function Employee(props) {
   return (
-    <div className="tw-m-2 tw-py-8 tw-px-8 tw-max-w-sm tw-bg-white tw-rounded-xl tw-shadow-lg tw-space-y-2 sm:tw-py-4 sm:tw-flex sm:tw-items-center sm:tw-space-y-0 sm:tw-space-x-6">
+    <div className="tw-min-w-[350px] tw-max-w-[350px]  tw-m-2 tw-py-8 tw-px-8 tw-max-w-sm tw-bg-white tw-rounded-xl tw-shadow-lg tw-space-y-2 sm:tw-py-4 sm:tw-flex sm:tw-items-center sm:tw-space-y-0 sm:tw-space-x-6">
       <img
         className="tw-object-cover tw-block tw-h-[100px] tw-w-[100px] tw-mx-auto tw-h-24 tw-rounded-full sm:tw-mx-0 sm:tw-shrink-0"
         src={props.img}
